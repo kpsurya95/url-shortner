b00b55ed49209055d28d4f55046441ec330bbd9e
ganesh0479 <ganesh0479@gmail.com>
2020-09-05 22:33:29 +0530

    added jacoco for code coverage

---
+++ src/main/java/com/sample/urlshortner/UrlShortner.java
@@ -0,0 +1,12 @@
+package com.sample.urlshortner;
+
+public class UrlShortner {
+  public boolean isValueExist() {
+    String str = "Sivaganesh";
+    if (str.equals("Sivaganesh")) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
--- src/test/java/com/sample/urlshortner/UrlShortnerApplicationTests.java
+++ src/test/java/com/sample/urlshortner/UrlShortnerApplicationTests.java
@@ -3,11 +3,14 @@ package com.sample.urlshortner;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class UrlShortnerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+  @Test
+  void contextLoads() {
+    UrlShortner urlShortner = new UrlShortner();
+    assertTrue(urlShortner.isValueExist());
+  }
 }
