9e9a6bbe43f912f4ea10da82c7c75ea3f5e2161f
ganesh0479 <ganesh0479@gmail.com>
2020-09-07 22:24:41 +0530

    added group

--- src/main/java/com/sample/urlshortner/UrlShortnerController.java
+++ src/main/java/com/sample/urlshortner/UrlShortnerController.java
@@ -1,15 +1,22 @@
 package com.sample.urlshortner;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1/url-shortner")
 public class UrlShortnerController {
 
   @GetMapping("/hello")
-  public String sayHello() {
-    return "Hello Sivaganesh";
+  public ResponseEntity sayHello() {
+    return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY)
+        .location(URI.create("https://www.google.com"))
+        .build();
   }
 }
